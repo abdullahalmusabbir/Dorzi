2847227eb313a30f5dbe4eabe84c5d255d532193
Niloy Ranjan Nath <niloynath003@gmail.com>
2025-07-28 23:20:08 +0600

    create embroidery app and added attribute

---
+++ embroidery/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmbroideryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'embroidery'
---
+++ embroidery/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Embroidery(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='embroidery/', blank=True, null=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
